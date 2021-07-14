562d048dbf48719b70b931bbee6f80b2c8ad79d1
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-07-14 15:50:53 +0900

    main2

---
+++ main2.py
@@ -0,0 +1,13 @@
+from prefect import task, Flow, Parameter
+
+@task
+def say_hello(name):
+    print(f"Hello {name}!")
+
+
+with Flow("Say Hi!") as flow:
+    name = Parameter("name")
+    say_hello(name)
+
+
+state = flow.run(name="hoge")
